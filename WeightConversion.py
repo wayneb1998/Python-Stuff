93c8632cd2ce382c40985481cb55959408d2659a
wayneb1998 <email8087@gmail.com>
2024-09-27 13:16:41 +0100

    Update WeightConversion.py

--- WeightConversion.py
+++ WeightConversion.py
@@ -6,7 +6,10 @@ if unit.upper() == "L":
 elif unit.upper() == "K":
     conversion2 = weight / 0.45
     print(f'Your weight in KG is: {conversion2} KGs ')
-# This project was created with credit to https://www.youtube.com/watch?v=_uQrJ0TkZlc&list=PL2YqTZEeE8_lWnyeLl3_YNxkddUXvx3cU&index=5 #
+# This project was created with credit to https://www.youtube.com/watch?v=_uQrJ0TkZlc&list=PL2YqTZEeE8_lWnyeLl3_YNxkddUXvx3cU&index=5 
+# Known errors/mistakes: 1. unecessary to add a new variable (conversion2), as the variable used depends on if the statement is correct 
+# 2. cosmetic but no need for a : after the question mark. Also cosmetic but (K)gs instead of Kg.
+
 
 
 
